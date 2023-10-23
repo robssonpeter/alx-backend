49fa41872e2b44de47edf8cfe6ee63c0a1f1efb9
robssonpeter <peterrobsson@gmail.com>
2023-10-23 20:39:04 +0300

    still solving question 2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -13,9 +13,6 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     first_element = last_element - page_size
     return (first_element, last_element)
 
-if __name__ == "__main__":
-    print(index_range(8, 9))
-
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -37,35 +34,41 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """ This method is going to return the page a list of thing """
-            assert isinstance(page, int) and page > 0, AssertionError('AssertionError raised with negative values')
-            assert isinstance(page_size, int) and page_size > 0, AssertionError('AssertionError raised with negative values')
-            if page == 0:
-                 raise AssertionError('AssertionError raised with 0')
-            elif isinstance(page_size, int) is False:
-                raise AssertionError('AssertionError raised when page and/or page_size are not ints')
+        """ This method is going to return the page a list of thing """
+        ng_msg = 'AssertionError raised with negative values'
+        page_is_int = isinstance(page, int)
+        size_is_int = isinstance(page_size, int)
+        assert page_is_int and page > 0, AssertionError(ng_msg)
+        assert size_is_int and page_size > 0, AssertionError(ng_msg)
+        if page == 0:
+            raise AssertionError('AssertionError raised with 0')
+        elif isinstance(page_size, int) is False:
+            msg = """
+            AssertionError raised when page and/or page_size are not ints
+            """
+            raise AssertionError(msg)
 
-            indexes = index_range(page, page_size)
+        indexes = index_range(page, page_size)
 
-            data = self.dataset()
-            """ The inputs are out of range """
-            if (page > math.ceil(len(data)/page_size)):
-                 return []
+        data = self.dataset()
+        """ The inputs are out of range """
+        if (page > math.ceil(len(data) / page_size)):
+            return []
 
-            return data[indexes[0]: indexes[1]]
+        return data[indexes[0]: indexes[1]]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-         """ This method returns a dictionary of the result with some extra information """
-         data = self.get_page(page, page_size)
-         pages = math.ceil(len(self.dataset())/page_size)
-         next_page = page + 1 if page < pages else None
-         prev_page = page - 1 if page > 1 else None
+        """ Returns a dictionary of the result with some extra info """
+        data = self.get_page(page, page_size)
+        pages = math.ceil(len(self.dataset())/page_size)
+        next_page = page + 1 if page < pages else None
+        prev_page = page - 1 if page > 1 else None
 
-         return {
-              "page_size": len(data),
-              "page": page,
-              "data": data,
-              "next_page": next_page,
-              "prev_page": prev_page,
-              "total_pages": pages
-         }
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": pages
+        }
