e4c57884920f9e542dbd11e7c7eb83b77d9cb896
robssonpeter <peterrobsson@gmail.com>
2023-10-23 13:44:59 +0300

    solved the zeroth question

---
+++ x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from typing import Tuple
+""" The module to define a function index_range """
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ The function index range """
+    last_element = page * page_size
+    first_element = last_element - page_size
+    return (first_element, last_element)
