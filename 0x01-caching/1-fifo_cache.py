876fcbf9d9aa53a7a017ef3e3758b19fd8bfad25
Peter Mgembe <peterrobsson@gmail.com>
2023-10-25 08:26:49 +0300

    solved question 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" The caching module that uses FIFO Caching """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ The class that uses FIFO Caching """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """ ### The method for putting an entry into cache
+            Its quite interesting here
+        """
+        if key is not None and item is not None:
+            keys = list(self.cache_data.keys())
+            if len(keys) >= self.MAX_ITEMS:
+                """ Remove the first element """
+                first_key = keys[0]
+                print(f'DISCARD: {first_key}')
+                del self.cache_data[first_key]
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ The function to retrieve a certain key from cache"""
+        keys = self.cache_data.keys()
+        if key is None:
+            return None
+        if key not in keys:
+            return None
+        return self.cache_data[key]
