b755ec98870dd10fbbe8299a6dfe8fcd1423fa42
Peter Mgembe <peterrobsson@gmail.com>
2023-10-31 17:08:17 +0300

    solved question 5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -31,15 +31,29 @@ def get_locale() -> Union[str, None]:
 
 @app.route('/')
 def home() -> str:
+    users = {
+        1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+        2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+        3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+        4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+    }
     """ The home route for the application """
     home_title = gettext('home_title')
     home_header = gettext('home_header')
+    logged_in_as = gettext('logged_in_as')
+    not_logged_in = gettext('not_logged_in')
     data = {
         "home_header": home_header,
         "home_title": home_title,
     }
+    login_as = request.args.get('login_as')
+    keys = list(users.keys())
+    if login_as and int(login_as) in keys:
+        data['logged_in_as'] = users[int(login_as)]
+    else:
+        data["not_logged_id"] = not_logged_in
 
-    return flask.render_template('4-index.html', **data)
+    return flask.render_template('5-index.html', **data)
 
 
 if __name__ == "__main__":
