f14b1aaf850f50c01ca622d468314410248aabe5
Peter Mgembe <peterrobsson@gmail.com>
2023-10-31 11:42:11 +0300

    fixed some pycodestyle validations

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,11 +3,13 @@ from flask import Flask
 import flask
 """ The entry file for the web application """
 
+
 app = Flask(__name__, template_folder="templates/")
 
 @app.route('/')
 def home():
     return flask.render_template('0-index.html', name="Jackson")
+
 
 if __name__ == "__main__":
     app.run()
